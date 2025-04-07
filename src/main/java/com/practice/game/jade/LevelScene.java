4a10d44248a0a1faf4d90395cd648ff88d59d007
I-Ting <it.tsai.1113@gmail.com>
2025-04-07 15:52:33 +0800

    add change to white

--- src/main/java/com/practice/game/jade/LevelScene.java
+++ src/main/java/com/practice/game/jade/LevelScene.java
@@ -3,6 +3,9 @@ package com.practice.game.jade;
 public class LevelScene extends Scene{
     public LevelScene() {
         System.out.println("Level Scene");
+        Window.get().r = 1;
+        Window.get().g = 1;
+        Window.get().b = 1;
     }
 
     @Override
